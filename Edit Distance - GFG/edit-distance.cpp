a1a31a1359bc28f6f5d9f7e03bd38488cce45279
Kushagra Madhukar <madhukarkushagra@gmail.com>
2022-04-23 19:18:44 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -5,19 +5,39 @@ using namespace std;
  // } Driver Code Ends
 class Solution {
   public:
-    int dp[101][101];
-    int solve(int m, int n, string s, string t) {
-        if(m == 0) return n;
-        if(n == 0) return m;
-        if(dp[m][n] != -1) return dp[m][n];
-        if(s[m-1] == t[n-1]) return dp[m][n] = solve(m-1, n-1, s, t);
-        return dp[m][n] = 1 + min(min(solve(m, n-1, s, t), solve(m-1, n, s, t)), solve(m-1, n-1, s, t));
-    }
+    // int dp[101][101];
+    // int solve(int m, int n, string s, string t) {
+    //     if(m == 0) return n;
+    //     if(n == 0) return m;
+    //     if(dp[m][n] != -1) return dp[m][n];
+    //     if(s[m-1] == t[n-1]) return dp[m][n] = solve(m-1, n-1, s, t);
+    //     return dp[m][n] = 1 + min(min(solve(m, n-1, s, t), solve(m-1, n, s, t)), solve(m-1, n-1, s, t));
+    // }
     int editDistance(string s, string t) {
         int m = s.size();
         int n = t.size();
-        memset(dp, -1, sizeof(dp));
-        return solve(m, n, s, t);
+        int dp[m+1][n+1];
+        for(int i=0;i<=m;i++) {
+            for(int j=0;j<=n;j++) {
+                if(i==0) {
+                    dp[i][j] = j;
+                    continue;
+                }
+                else if(j==0) {
+                    dp[i][j] = i;
+                    continue;
+                }
+                else if(s[i-1] == t[j-1]) {
+                    dp[i][j] = dp[i-1][j-1];
+                    continue;
+                }
+                else {
+                    dp[i][j] =  1 + min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]);
+                }
+            }
+        }
+        return dp[m][n];
+        // return solve(m, n, s, t);
     }
 };
 
