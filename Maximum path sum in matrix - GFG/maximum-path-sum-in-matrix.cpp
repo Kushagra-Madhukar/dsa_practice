6e32062039f36f0ae43c1842330d329ed17680e8
Kushagra Madhukar <madhukarkushagra@gmail.com>
2022-06-09 02:05:06 +0530

    Added solution - LeetHub

--- Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
+++ Maximum path sum in matrix - GFG/maximum-path-sum-in-matrix.cpp
@@ -37,20 +37,20 @@ public:
         if(i<0 || j<0 || i>=N || j>=N) return 0;
         if(dp[i][j]!= -1) return dp[i][j];
         if(i==N-1) return Matrix[i][j];
-        int dy[] = {1, -1, 0};
-        int ans = INT_MIN;
-        for(int u=0;u<3;u++){
-            int newY = j + dy[u];
-            if(newY>=0 && newY<N){
-                int x = dp[i+1][newY] != -1 ? dp[i+1][newY] : recursiveMaxPath(i+1, newY, N, Matrix);
-                ans = max(ans, Matrix[i][j] + x);
-            }
-        }
-        return dp[i][j] = ans;
-        // int a = dp[i+1][j] != -1 ? dp[i+1][j] : Matrix[i][j] + recursiveMaxPath(i+1, j, N, Matrix);
-        // int b = dp[i+1][j-1] != -1 ? dp[i+1][j-1] : Matrix[i][j] + recursiveMaxPath(i+1, j-1, N, Matrix);
-        // int c = dp[i+1][j+1] != -1 ? dp[i+1][j+1] : Matrix[i][j] + recursiveMaxPath(i+1, j+1, N, Matrix);
-        // return dp[i][j] = max(max(a, b), c);
+        // int dy[] = {1, -1, 0};
+        // int ans = INT_MIN;
+        // for(int u=0;u<3;u++){
+        //     int newY = j + dy[u];
+        //     if(newY>=0 && newY<N){
+        //         int x = dp[i+1][newY] != -1 ? dp[i+1][newY] : recursiveMaxPath(i+1, newY, N, Matrix);
+        //         ans = max(ans, Matrix[i][j] + x);
+        //     }
+        // }
+        // return dp[i][j] = ans;
+        int a = dp[i+1][j] != -1 ? dp[i+1][j] : recursiveMaxPath(i+1, j, N, Matrix);
+        int b = dp[i+1][j-1] != -1 ? dp[i+1][j-1] : recursiveMaxPath(i+1, j-1, N, Matrix);
+        int c = dp[i+1][j+1] != -1 ? dp[i+1][j+1] : recursiveMaxPath(i+1, j+1, N, Matrix);
+        return dp[i][j] = max(max(a, b), c) + Matrix[i][j];
     }
     int maximumPath(int N, vector<vector<int>> Matrix){
         int maxSum = INT_MIN;
