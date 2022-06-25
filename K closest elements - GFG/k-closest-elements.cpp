0c091ffb5836836e040a89d078369a706e99b1f2
Kushagra Madhukar <madhukarkushagra@gmail.com>
2022-06-26 02:22:15 +0530

    Added solution - LeetHub

--- K closest elements - GFG/k-closest-elements.cpp
+++ K closest elements - GFG/k-closest-elements.cpp
@@ -7,39 +7,51 @@ using namespace std;
  // } Driver Code Ends
 //User function template for C++
 
+class comparator{
+    public:
+    int operator()( pair<int, int> &p1, pair<int, int> &p2){
+        if(p1.first == p2.first) return p1.second<p2.second;
+        else return p1.first>p2.first;
+    }
+};
+
 class Solution{   
 public:
     vector<int> printKClosest(vector<int> arr, int n, int k, int x) {
         vector<int> ans;
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
+        priority_queue<pair<int, int>, vector<pair<int, int>>, comparator> minHeap;
         for(int i=0;i<n;i++){
             int diff = abs(arr[i]-x);
             if(diff!=0) minHeap.push({diff, arr[i]});
         }
-        int i = 0;
-        while(i<k && !minHeap.empty()){
-            int absdiff = minHeap.top().first;
-            int val = minHeap.top().second;
+        for(int i=0;i<k;i++){
+            ans.push_back(minHeap.top().second);
             minHeap.pop();
-            if(!minHeap.empty() && minHeap.top().first == absdiff) {
-                priority_queue<int> maxHeap;
-                maxHeap.push(val);
-                while(!minHeap.empty() && minHeap.top().first == absdiff){
-                    int currVal = minHeap.top().second;
-                    minHeap.pop();
-                    maxHeap.push(currVal);
-                }
-                while(i<k && !maxHeap.empty()){
-                    ans.push_back(maxHeap.top());
-                    maxHeap.pop();
-                    i++;
-                }
-            }
-            else {
-                ans.push_back(val);
-                i++;
-            }
         }
+        // int i = 0;
+        // while(i<k && !minHeap.empty()){
+        //     int absdiff = minHeap.top().first;
+        //     int val = minHeap.top().second;
+        //     minHeap.pop();
+        //     if(!minHeap.empty() && minHeap.top().first == absdiff) {
+        //         priority_queue<int> maxHeap;
+        //         maxHeap.push(val);
+        //         while(!minHeap.empty() && minHeap.top().first == absdiff){
+        //             int currVal = minHeap.top().second;
+        //             minHeap.pop();
+        //             maxHeap.push(currVal);
+        //         }
+        //         while(i<k && !maxHeap.empty()){
+        //             ans.push_back(maxHeap.top());
+        //             maxHeap.pop();
+        //             i++;
+        //         }
+        //     }
+        //     else {
+        //         ans.push_back(val);
+        //         i++;
+        //     }
+        // }
         return ans;
     }
 };
