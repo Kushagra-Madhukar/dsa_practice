a853e79b5c687dde6ec3f5c895a521e50126b94d
Kushagra Madhukar <madhukarkushagra@gmail.com>
2022-06-27 02:42:32 +0530

    Added solution - LeetHub

--- Find median in a stream - GFG/find-median-in-a-stream.cpp
+++ Find median in a stream - GFG/find-median-in-a-stream.cpp
@@ -13,29 +13,52 @@ class Solution
     double median;
     void insertHeap(int &x)
     {
-        if(s.size() == l.size()) s.push(x);
-        else l.push(x);
+        // if(s.size() == l.size()) s.push(x);
+        // else l.push(x);
+        // balanceHeaps();
+        if(s.size() == 0 || s.top()>x){
+            s.push(x);
+        }
+        else{
+            l.push(x);
+        }
         balanceHeaps();
     }
     
     //Function to balance heaps.
     void balanceHeaps()
     {
-        if(s.size() > 0 && l.size() > 0 && s.top()>l.top()){
-            double x = s.top();
-            double y = l.top();
-            s.pop(); 
+        // if(s.size() > 0 && l.size() > 0 && s.top()>l.top()){
+        //     double x = s.top();
+        //     double y = l.top();
+        //     s.pop(); 
+        //     l.pop();
+        //     s.push(y);
+        //     l.push(x);
+        // }
+        if(s.size()>l.size()+1){
+            l.push(s.top());
+            s.pop();
+        }
+        else if(l.size()>s.size()){
+            s.push(l.top());
             l.pop();
-            s.push(y);
-            l.push(x);
         }
     }
     
     //Function to return Median.
     double getMedian()
     {
-        if(s.size() == l.size()) return median = (s.top() + l.top())/2;
-        else return median = s.top();
+        // if(s.size() == l.size()) return median = (s.top() + l.top())/2;
+        // else return median = s.top();
+        double med;
+         if(l.size()==s.size()){
+             med=(s.top()+l.top())/2.0;
+         }
+         else{
+             med=s.top();
+         }
+         return med;
     }
 };
 
